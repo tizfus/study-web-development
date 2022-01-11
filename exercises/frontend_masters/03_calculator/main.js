ed2d0c484edbd1c1730c58de0adb40e0ee52e089
Tiziano Fusini <tiziano.fusini@it.clara.net>
2022-01-11 13:02:53 +0100

    refactors

--- exercises/frontend_masters/03_calculator/main.js
+++ exercises/frontend_masters/03_calculator/main.js
@@ -1,60 +1,56 @@
 window.onload = function(){
-    let calculation = []
+    let operations = []
     let clean = true
 
-    const keys = Array.prototype.slice.call(
-        document.getElementsByClassName('key')
-    )
-
+    const keys = Array.prototype.slice.call(document.getElementsByClassName('key'))
     const numbers = keys.filter(x => x.parentNode.classList.contains('numbers'))
     const operators = keys.filter(x => x.parentNode.classList.contains('operators'))
-
     const display = document.getElementsByClassName('display')[0]
-
-    for(let number of numbers) {
-        number.onclick = function() {
-            if(clean) {
-                clear()
-            }
-            display.textContent += this.textContent
-        }
-    }
-
-    operators.forEach(operator => 
-        operator.onclick = function () {
-            calculation.push(display.textContent)
-            calculation.push(operation(operator.textContent))
-            console.log(calculation)
-            clear()
-        }
-    );
-
     const equalsOperator = operators.filter(operator => operator.textContent == '=')[0]
 
-    equalsOperator.onclick = () => {
-        calculation.push(display.textContent)
-        clean = true
-        display.textContent = calculate(0, calculation)
+
+
+    numbers.forEach(number => number.onclick = appendDigit)
+    operators.forEach(operator => operator.onclick = appendOperation)
+    equalsOperator.onclick = printResult
+
+
+
+    function appendDigit() {
+        if(clean) { clear() }
+        display.textContent += this.textContent
     }
 
+    function appendOperation() {
+        operations.push(display.textContent)
+        operations.push(operation(this.textContent))
+        console.log(operations)
+        clear()
+    }
 
-    function calculate(initValue, calcs){
-        console.debug(`${initValue} # ${calcs}`)
+    function printResult(){
+        operations.push(display.textContent)
+        clean = true
+        display.textContent = calculate(0, operations)
+    }
+
+    function calculate(initValue, operations){
+        console.debug(`${initValue} # ${operations}`)
         
-        if(calcs.length == 0) {
+        if(operations.length == 0) {
             return 0
         }
         
-        const item = calcs.shift()
+        const item = operations.shift()
 
         if(typeof(item) == 'function'){
             const operation = item
-            const number = toInt(calcs.shift())
+            const number = toInt(operations.shift())
             
             return operation(initValue, number)
         }
         
-        return calculate(toInt(item), calcs)
+        return calculate(toInt(item), operations)
     
 
     }
